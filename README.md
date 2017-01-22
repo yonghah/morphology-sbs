3ef2056c1beec4db2b9634465d9626171c9962ec
Yongha Hwang <yongha.hwang@gmail.com>
2017-01-22 09:25:29 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Urban Morphology Side-by-Side
+
+See the demo:
+
+
+This web page provides morphological comparison acrss world cities in a easy
+way. The scale across maps are kept always same. You can change scale by using
+zoom slider at the top left corder or wheel scrolling in the map.
+
+The map is styled and rendered using Mapbox-gl.js.
+
+If you prefer to see one large map with this style, use this page:
+
+http://www-personal.umich.edu/~yonghah/gstreet/
---
+++ multi.js
@@ -0,0 +1,104 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWhnbm95IiwiYSI6ImZIcGRiZjgifQ.pL1SaB8gHyl-L2yolSl5Qw';
+
+
+var maps = [];
+var current_id = 0;
+var current_zoom = 12;
+var map_size = 400;
+//map_ids.map(addMap);
+//addCity(maps[0]);
+//addCity(maps[1]);
+
+function addCity(query){
+    var map_id = current_id;
+    current_id += 1;
+    $("#container").append(
+        $("<div>", {'id':'container_' + map_id, 'class':'city'}));
+    var map = createMap(map_id);
+    var map_info = {'id':map_id, 'map':map};
+    updateMapSize(map_info);
+    maps.push(map_info);
+    addr_search(map_info, query);
+}
+
+function createMap(map_id){
+    $("#container_" + map_id)
+        .append($('<div>', {'class':'map', 'id':"mapbox_" + map_id}));
+    var map = new mapboxgl.Map({
+        container: "mapbox_" + map_id,
+        style: 'mapbox://styles/ahgnoy/cixpb7f3q001x2sojafk0x4af',
+        center: [127.03, 37.5],
+        minZoom: 3,
+        zoom: current_zoom
+    });
+    //map.addControl(new mapboxgl.NavigationControl());
+    map.on("zoomend", function(){
+        current_zoom = map.getZoom();
+        maps.map(
+            function(m){
+                m.map.setZoom(current_zoom)
+            }
+        );
+    });    
+    return map;
+}
+
+function addCityClick(){
+    var inp = $('#main_search').val();
+    addCity(inp);
+}
+
+
+
+function addr_search(map_info, query) {
+    $.getJSON('http://nominatim.openstreetmap.org/search?format=json&limit=5&q=' + query, function(data) {
+        console.log(data[0]);
+        if (data[0]) {
+            map_info.display_name = data[0].display_name;
+            map_info.map.panTo(
+                {lat:Number(data[0].lat), lng:Number(data[0].lon)}
+            );
+        } else {
+            map_info.display_name = "Cannot find the city"
+        }
+        addInfo(map_info);
+   });
+}
+
+function addInfo(map_info) {
+    console.log(map_info);
+    $("#container_" + map_info.id).append(
+        $('<h5>')
+        .addClass("text-center")
+        .html(map_info.display_name)
+        .append($('<button>', {'class':'btn btn-default btn-xs'})
+            .html('x')
+            .click(function(){
+                $("#container_" + map_info.id).remove()
+            })
+        )
+    );
+}
+
+function zoomUpdate(value) {
+    current_zoom = value;
+    maps.map(
+        function(m){
+            m.map.setZoom(current_zoom)
+        }
+    );
+    $("#globalZoomOutput").html(value);
+}
+
+function updateMapsSize(value) {
+    map_size = value;
+    maps.map(updateMapSize);
+    $("#mapSizeOutput").html(value);
+}
+
+function updateMapSize(map_info) {
+    var div = $("#mapbox_" + map_info.id);
+    div.css("width", map_size);
+    div.css("height", map_size);
+    map_info.map.resize();
+}
